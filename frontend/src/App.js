146863bfe08b4df49f66d6ac9d4e066523b23c34
TangadeSandesh <tangadesandesh2001@gmail.com>
2024-07-09 00:14:52 +0530

    New package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   refreshList = () => {
     axios
-      .get("http://127.0.0.1:8000/api/todos/")
+      .get("https://todo-app-postgresql-six.vercel.app/api/todos/")
       .then((res) => this.setState({ todoList: res.data }))
       .catch((err) => console.log(err));
   };
@@ -37,18 +37,18 @@ class App extends Component {
 
     if (item.id) {
       axios
-        .put(`http://127.0.0.1:8000/api/todos/${item.id}/`, item)
+        .put(`https://todo-app-postgresql-six.vercel.app/api/todos/${item.id}/`, item)
         .then((res) => this.refreshList());
       return;
     }
     axios
-      .post("http://127.0.0.1:8000/api/todos/", item)
+      .post("https://todo-app-postgresql-six.vercel.app/api/todos/", item)
       .then((res) => this.refreshList());
   };
 
   handleDelete = (item) => {
     axios
-      .delete(`http://127.0.0.1:8000/api/todos/${item.id}/`)
+      .delete(`https://todo-app-postgresql-six.vercel.app/api/todos/${item.id}/`)
       .then((res) => this.refreshList());
   };
 
